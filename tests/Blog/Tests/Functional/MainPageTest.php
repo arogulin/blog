cf2e4d57db0e3f64cbec8d6620d0254bc4561ada
Aleksey A. Rogulin <aleksey.php@gmail.com>
2013-03-26 22:04:23 +0400

    blog-8. Begin test for pagination on main page.

--- tests/Blog/Tests/Functional/MainPageTest.php
+++ tests/Blog/Tests/Functional/MainPageTest.php
@@ -4,12 +4,12 @@ namespace Blog\Tests\Functional;
 use Silex\WebTestCase;
 
 class TestMainPage extends WebTestCase {
-
+    
     public function createApplication() {
         require __DIR__ . '/../../../../config/bootstrap.php';
         $app['debug'] = true;
         $app['exception_handler']->disable();
-
+        
         return $app;
     }
 
@@ -19,7 +19,38 @@ class TestMainPage extends WebTestCase {
 
         // Server should answer with 200 HTTP-code
         $this->assertTrue($client->getResponse()->isOk());
-        // There are config.pagination.posts_main_page should be
+    }
+
+    public function testMainPagePagination() {
+        $client = $this->createClient();
+        echo '<pre>';
+        var_dump($client);
+        echo '</pre>';
+        die();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
         
+        return true;
+        
+        // There are config.pagination.posts_main_page should be
+        $expectedPostsCount = $this->app['config']['pagination']['posts_main_page'];
+        $this->assertCount($expectedPostsCount, $crawler->filter('div.post'));
+        
+        return true;
+        /*
+        $changedPostCount = 5;
+        $this->app['config']['pagination']['posts_main_page'] = $changedPostCount;
+
+        
+        $client = $this->createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+
+        $this->assertCount($changedPostCount, $crawler->filter('div.post'));
+*/
+
     }
 }
